ea786a0e27feddc870fee45746df43fc46fea9b2
Fish <>
2021-04-13 18:22:17 +0800

    q1 answer

--- q1.py
+++ q1.py
@@ -1,15 +1,16 @@
 def fib(n):
     f = dict()
-    f[1] = 2
+    f[0] = 1
+    f[1] = 1
     f[2] = 2
 
-    if n == 1:
+    if n == 0 or n == 1:
         return 1
     elif n == 2:
         return 2
 
     for i in range(3, n + 1):
-        f[i] = f[i - 1] + f[i - 2]
+        f[i] = f[i - 1] + f[i - 2] + f[i - 3]
 
     return f[n]
 
